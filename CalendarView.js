e147037860da83cec7b7e27d11007c5c096be088
Mario De Jesus Guzman Cabral <144813332+yudas-programador@users.noreply.github.com>
2025-04-06 12:29:33 -0400

    Create CalendarView.js

---
+++ CalendarView.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CalendarView = () => {
+  return (
+    <div>
+      <h2>Calendario</h2>
+      <p>Hábitos para el día seleccionado</p>
+      <p>No hay hábitos programados para esta fecha</p>
+    </div>
+  );
+};
+
+export default CalendarView;
